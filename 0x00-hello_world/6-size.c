5a46ed1ef7edf7ffc201c076c34dde76f3ee3759
Michael <micaelsosa@fernandosor.edu.co>
2019-06-19 12:06:14 -0500

    Organizing files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+<h1>This is CISFUN all about basics on C</h1>

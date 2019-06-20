659deb02f49e9a1175637b4b72398eef21269f03
Michael <micaelsosa@fernandosor.edu.co>
2019-06-20 12:39:14 -0500

    Printing numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,14 @@
 int main(void)
 {
 	int n;
+	char ln = '\n';
 
-        for(n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		printf ("%d", n);
+		printf("%d", n);
 	}
+
+	putchar(ln);
+
 	return (0);
 }

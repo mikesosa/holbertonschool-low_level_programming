567a89be051c1b41203b350389feafe7b4bff648
Michael <micaelsosa@fernandosor.edu.co>
2019-06-25 12:57:25 -0500

    Sum of two digits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - sum of two integers.
+ * @x: Integer
+ * @y: Integer
+ * Return: (z) the result
+ */
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}

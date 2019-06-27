3ab788f5c8580c9b7709864ec7b6f61e0712e6b2
Michael <micaelsosa@fernandosor.edu.co>
2019-06-27 15:24:48 -0500

    FIX

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,54 @@
 #include "holberton.h"
 #include <stdio.h>
+
+/**
+ * _check_div - printing numbers with putchar.
+ * @x: number to print
+ * Return: no return.
+ */
+int _check_div(int x)
+{
+	int div;
+
+	div = 1;
+	while (div <= x)
+	{
+		div = div * 10;
+	}
+	return (div);
+}
+/**
+ * _check_div_neg - printing numbers with putchar.
+ * @x: number to print
+ * Return: no return.
+ */
+int _check_div_neg(int x)
+{
+	int div;
+
+	div = 1;
+	while (div <= x)
+	{
+		div = div * 10;
+	}
+	_putchar('-');
+	return (div);
+}
+/**
+ * _check_mod - printing numbers with putchar.
+ * @x: number to print
+ * @div: number to print
+ * Return: no return.
+ */
+int _check_mod(int x, int div)
+{
+	do {
+		div = div / 10;
+		_putchar(48 + (x / div));
+		x = x % div;
+	} while (x);
+	return (x);
+}
 /**
  * print_number - printing numbers with putchar.
  * @n: number to print
@@ -7,49 +56,21 @@
  */
 void print_number(int n)
 {
-
-	if (n != 0)
+	if (n > 9)
 	{
-		if (n < 0)
-		{
-			n = n * -1;
-			int div;
+		int x = n;
+		int div = _check_div(x);
 
-			div = 1;
-			while(div <= n)
-			{
-				div = div * 10;
-			}
-			// for (div = 1; div <= n; div *= 10)
-			// { ; }
-			_putchar('-');
+		_check_mod(x, div);
+	} else if (n < 0)
+	{
+		int x = n * (-1);
+		int div = _check_div_neg(x);
 
-			do {
-				div = div / 10;
-				_putchar(48 + (n / div));
-				n = n % div;
-			} while (n);
-
-
-		} else
-		{
-			int div;
-
-			div = 1;
-			while(div <= n)
-			{
-				div = div * 10;
-			}
-			// for (div = 1; div <= n; div *= 10)
-			// { ; }
-			do {
-				div = div / 10;
-				_putchar(48 + (n / div));
-				n = n % div;
-			} while (n);
-		}
+		_check_mod(x, div);
 	} else
 	{
 		_putchar(48 + n);
 	}
+
 }

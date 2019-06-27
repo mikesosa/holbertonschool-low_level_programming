67684368a3c1af5b0bce7881d2abf4fe41add942
Michael <micaelsosa@fernandosor.edu.co>
2019-06-27 12:08:42 -0500

    Fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,10 +8,15 @@
 void print_line(int n)
 {
 	int x;
-
-	for (x = 0; x <= n; x++)
+	if(n > 0)
 	{
-		_putchar('_');
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	} else
+	{
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

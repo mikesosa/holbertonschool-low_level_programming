790d3f04dd40298593dba6fa1610b142122b1ed8
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-02 18:15:00 +0000

    HOPE for this one

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * puts_half - function to reverse strings.
+ * @str: the string
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+
+	int x;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+
+
+	if (x % 2 == 0)
+	{
+
+		x /= 2;
+		for (; str[x] != '\0' ; x++)
+		{
+			_putchar(str[x]);
+		}
+
+	} else
+	{
+		x = (x - 1) / 2;
+		for (; str[x] != '\0' ; x++)
+		{
+			_putchar(str[x]);
+		}
+	}
+
+	_putchar('\n');
+
+}

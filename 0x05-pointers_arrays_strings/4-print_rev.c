3b2f6ffdb0fa7e6a54e080ce0e6953f6f9b0e7cd
Michael <micaelsosa@fernandosor.edu.co>
2019-07-02 09:17:43 -0500

    Hope

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function to print strings in reverse.
+ * @s: the string
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+
+	int x;
+	int ld;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	for (ld = x; ld >= 0; ld--)
+	{
+		_putchar(s[ld]);
+	}
+
+	_putchar('\n');
+}

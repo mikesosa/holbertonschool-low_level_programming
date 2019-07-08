37ff45551325afc0bf30ee24341b87a1345ee065
Michael <micaelsosa@fernandosor.edu.co>
2019-07-08 10:28:19 -0500

    5th one, flying today

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - finding a char in an array
+ * @s: source
+ * @accept: the only chars accepted
+ * Return: the new value of s.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; s[k] != '\0'; k++)
+		{
+			if (s[i] == accept[k])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
+}

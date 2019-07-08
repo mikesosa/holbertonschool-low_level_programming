47151c017b6e24e6dcb5fdb5d2c40350a0868ec4
Michael <micaelsosa@fernandosor.edu.co>
2019-07-08 15:53:52 -0500

    Lastone

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - changing the values of chars
+ * @s: source matrix
+ * @to: change it to
+ * Return: the new value of s.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

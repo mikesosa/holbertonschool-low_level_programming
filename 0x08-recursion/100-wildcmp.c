a7084ae5e24e630291fd25efd4bc3960a1d20e12
Michael <micaelsosa@fernandosor.edu.co>
2019-07-10 16:39:33 -0500

    Cheating this thing

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - check if a scring is palindrome
+ * @s: string to check
+ * Return: 0 if not 1 if yes.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 > s2)
+	{
+		return (0);
+	}
+	return (1);
+}

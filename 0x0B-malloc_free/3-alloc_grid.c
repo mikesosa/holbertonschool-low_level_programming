bb32f3a96b5dc08f8035dba57b92aeac6dfe8cfd
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-16 18:50:19 +0000

    Hope

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - returns a pointer of two dimensional array
+ * @width: Source widht of matrix
+ * @height: Source height of matrix
+ * Return: @dest which is the pointer that points to a 2 dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int j = 0;
+	int **dest;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+	dest = malloc(height * sizeof(int));
+
+	if (dest == 0)
+		return (0);
+	while (i < height)
+	{
+		dest[i] = malloc(width * sizeof(int));
+		i++;
+	}
+
+	if (dest == 0)
+		return (0);
+	i = 0;
+	while (i < height)
+	{
+		while (j < width)
+		{
+			dest[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+
+	return (dest);
+}

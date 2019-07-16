d2d99f81a6dcbe1035873d0be79a9b22d3dd142e
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-16 19:40:43 +0000

    Test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,17 +14,15 @@ int **alloc_grid(int width, int height)
 	int **dest;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
+		return (0);
 	dest = malloc(height * sizeof(int *));
 
 	if (dest == NULL)
-		free(dest);
 		return (0);
 	while (i < height)
 	{
 		dest[i] = malloc(width * sizeof(int));
 		if (dest[i] == NULL)
-			free(dest);
 			return (0);
 		i++;
 	}

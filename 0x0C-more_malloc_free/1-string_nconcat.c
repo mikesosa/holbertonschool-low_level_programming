db2df5807db71ab730430e832fe2cd4dd5f89f64
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-18 18:38:45 +0000

    Give up

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,5 @@
 #include "holberton.h"
 /**
- * *same_size - returns a pointer with concatenation of n chars
- * @s1: first string
- * @s2: second string
- * @pointer: number of chars to pass into p
- * Return: p, a pointer to the new space of memory
- */
-char *same_size(char *s1, char *s2, char *pointer)
-{
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int k = 0;
-
-	while (s1[i])
-	{
-		pointer[k] = s1[i];
-		i++;
-		k++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		pointer[k] = s2[j];
-		j++;
-		k++;
-	}
-	pointer[k] = '\0';
-	return (pointer);
-}
-
-/**
 * string_nconcat - concatenates 2 strings
 * @s1: string one
 * @s2: string two
@@ -39,41 +9,41 @@ char *same_size(char *s1, char *s2, char *pointer)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int k = 0;
+	unsigned int i = 0, j = 0, k = 0, lS1 = 0, lS2 = 0, total_l = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (n >= j)
-		pointer = malloc((i + j + 2) * sizeof(char));
-	if (n < j)
-		pointer = malloc((i + n + 1) * sizeof(char));
+	while (s1[lS1])
+		lS1++;
+	while (s2[lS2])
+		lS2++;
+	if (n >= lS2)
+		n = lS2;
+	total_l = lS1 + n + 1;
+	pointer = malloc(total_l * sizeof(char));
 	if (pointer == NULL)
 		return (NULL);
-	if (n >= j)
-		pointer = same_size(s1, s2, pointer);
 	i = 0;
-	while (s1[i])
-	{
-		pointer[k] = s1[i];
-		i++;
-		k++;
-	}
 	j = 0;
-	while (n)
+	while (i < total_l)
 	{
-		pointer[k] = s2[j];
-		n--;
-		j++;
-		k++;
+		if (i < lS1)
+		{
+			pointer[i] = s1[j];
+			i++;
+			j++;
+		} else if (i < total_l - 1)
+		{
+			pointer[i] = s2[k];
+			i++;
+			k++;
+		} else if (i < total_l)
+		{
+			pointer[i] = '\0';
+			i++;
+		}
 	}
-	pointer[k] = '\0';
 	return (pointer);
 }

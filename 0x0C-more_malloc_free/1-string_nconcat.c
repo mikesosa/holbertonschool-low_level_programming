c5f942c747873538e15f8a3be4932584dd99bbdd
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-18 17:44:13 +0000

    Don't know

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,72 +13,81 @@ int size(char *s)
 		i++;
 	return (i);
 }
-
 /**
  * *print_n - returns a pointer with concatenation of n chars
  * @s1: first string
  * @s2: second string
- * @lengthS1: length of string s1
+ * @pointer: the size of the new pointer
  * @n: number of chars to pass into p
  * Return: p, a pointer to the new space of memory
  */
-char *print_n(char *s1, char *s2, int lengthS1, unsigned int n)
+char *print_n(char *s1, char *s2, unsigned int n, char *pointer)
 {
-	char *p;
 	unsigned int i = 0;
 	unsigned int j = 0;
+	unsigned int k = 0;
 
-	p = malloc(sizeof(char) * (lengthS1 + n + 1));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		p[i] = s1[i];
-	while (j < n)
+	i = 0;
+	while (s1[i])
 	{
-		p[i] = s2[j];
-		j++;
+		pointer[k] = s1[i];
 		i++;
+		k++;
 	}
-	p[i] = '\0';
-	return (p);
+	j = 0;
+	while (n)
+	{
+		pointer[k] = s2[j];
+		n--;
+		j++;
+		k++;
+	}
+	pointer[k] = '\0';
+	return (pointer);
 }
 /**
  * *string_nconcat - simply concatenates n chars from s2 with s1 to p
  * @s1: first string
  * @s2: second string
  * @n: number of chars to pass into p
- * Return: p, a pointer to the new space of memory
+ * Return: pointer, a pointer to the new space of memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int lengthS1 = 0;
-	unsigned int lengthS2 = 0;
-	int i = 0;
+	char *pointer;
+	unsigned int i = 0;
 	unsigned int j = 0;
+	unsigned int k = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	lengthS1 = size(s1);
-	lengthS2 = size(s2);
-	if (n >= lengthS2)
+	i = size(s1);
+	j = size(s2);
+	if (n >= j)
+		pointer = malloc((i + j + 2) * sizeof(char));
+	if (n < j)
+		pointer = malloc((i + n + 1) * sizeof(char));
+	if (pointer == NULL)
+		return (NULL);
+	if (n >= j)
 	{
-		p = malloc(sizeof(char) * (lengthS1 + lengthS2) + 1);
-		if (p == NULL)
-			return (NULL);
-		for (i = 0; s1[i] != '\0'; i++)
-			p[i] = s1[i];
+		i = 0;
+		while (s1[i])
+		{
+			pointer[k] = s1[i];
+			i++;
+			k++;
+		}
+		j = 0;
 		while (s2[j])
 		{
-			p[i] = s2[j];
+			pointer[k] = s2[j];
 			j++;
-			i++;
+			k++;
 		}
-		p[i] = '\0';
-		return (p);
+		pointer[k] = '\0';
 	}
-
-	return (print_n(s1, s2, lengthS1, n));
+	return (print_n(s1, s2, n, pointer));
 }

6fcbdce50fa6a5d20417b663822e557bc6f3026b
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-22 17:36:32 +0000

    Creating space for every string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,29 @@
 #include "dog.h"
 
 /**
+ *_strcreator - creates and duplicates the new string in the struct
+ * @str: char to the string
+ * Return: @ptr, the new alocatec string
+ */
+char *_strcreator(char *str)
+{
+	char *ptr;
+	unsigned int length, n;
+
+	length = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[length] != '\0')
+		length++;
+	ptr = malloc((length + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (n = 0; n <= length; n++)
+		ptr[n] = str[n];
+	return (ptr);
+}
+
+/**
  * new_dog - initializes a new dog structure
  * @name: name of dog
  * @age: age of dog
@@ -16,14 +39,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	n_doggie = malloc(sizeof(dog_t));
 	if (n_doggie == NULL)
 		return (NULL);
-	n_doggie->name = name;
+	n_doggie->name = _strcreator(name);
 	n_doggie->age = age;
-	n_doggie->owner = owner;
+	n_doggie->owner = _strcreator(owner);
 	if (n_doggie->name == NULL || n_doggie->owner == NULL)
 	{
 		free(n_doggie->name);
 		free(n_doggie->owner);
 		free(n_doggie);
-	}	
+	}
 	return (n_doggie);
 }

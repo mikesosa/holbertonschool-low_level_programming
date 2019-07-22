fff43180336c5ba556dd94ef1b367fc236d556c2
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-22 16:05:00 +0000

    Adding a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - initializes a new dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: @n_doggie, pointer to the new dog struct.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_doggie;
+
+	n_doggie = malloc(sizeof(dog_t));
+	if (n_doggie == NULL)
+		return (NULL);
+	n_doggie->name = name;
+	n_doggie->age = age;
+	n_doggie->owner = owner;
+	free(n_doggie);
+	return (n_doggie);
+}

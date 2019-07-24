115658d71239e72daeaf0070a77e2c7d5d3e88d9
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-24 15:14:54 +0000

    If this passes all checks! I'll cry

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+
+/**
+ * main - prints the result of a basic operation
+ * @argc: length of args passed to the program
+ * @argv: array of arguments
+ * Return: Error if more or less args, Error if
+ * module by 0 or divided y 0.
+ **/
+int main(int argc, char **argv)
+{
+	int result;
+	int val1 = 0;
+	int val2 = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	val1 = atoi(argv[1]);
+	val2 = atoi(argv[3]);
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && val2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	switch (argv[2][0])
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			result = get_op_func(argv[2])(val1, val2);
+			break;
+		default:
+			printf("Error\n");
+			exit(99);
+	}
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sums two ints
+ * @a: int a
+ * @b: int b
+ * Return: the sum
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two ints
+ * @a: int a
+ * @b: int b
+ * Return: the substraction
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two ints
+ * @a: int a
+ * @b: int b
+ * Return: the multiplication
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two ints
+ * @a: int a
+ * @b: int b
+ * Return: the division
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mods two ints
+ * @a: int a
+ * @b: int b
+ * Return: the module
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

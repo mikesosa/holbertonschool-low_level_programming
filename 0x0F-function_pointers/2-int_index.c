f72140fc025960b4ca383fb2baab54bc5f41add7
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-24 08:40:36 +0000

    Easy???

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns an index when comparision is true
+ * @array: the array we work with
+ * @size: size of the array
+ * @cmp: comparision we have to perform
+ * Return: @i if cmp is true, -1 if not found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0 && cmp)
+	{
+		while (size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			size--;
+			i++;
+		}
+	}
+	return (-1);
+}

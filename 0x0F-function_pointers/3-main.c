c61e47fa2f1508bc944e6fc7ac4e2079cbc3bbb2
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-24 17:32:37 +0000

    When next to operator diferent null

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,22 +28,13 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 
-	switch (argv[2][0])
-	{
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-		case '%':
-			result = get_op_func(argv[2])(val1, val2);
-	}
-
 	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
+	result = get_op_func(argv[2])(val1, val2);
 	printf("%d\n", result);
 	return (0);
 }

e5a9fc61bfd140ca65804a4112fcd55b59e2efd4
mikesosa <micaelsosa@fernandosor.edu.co>
2019-07-26 17:48:07 +0000

    Third Task: Printing all the strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints only the strings
+ * @separator: the thing printed between the strings
+ * @n: number of args passed
+ * Return: Nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i = 0;
+	char *str;
+
+	va_start(valist, n);
+	if (separator != NULL)
+	{
+		while (i < n)
+		{
+			str = va_arg(valist, char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
+			i++;
+			if (i < n)
+				printf("%s", separator);
+		}
+		printf("\n");
+	}
+	va_end(valist);
+}

ce2fba5323bb714097808be20e0a4842f300bebd
mikesosa <micaelsosa@fernandosor.edu.co>
2019-08-06 10:57:36 +0000

    Secure guards cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
 		return (NULL);
+	new->n = n;
 	if (idx == 0)
 	{
 		new->next = *head;
@@ -23,7 +24,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (*head == 0)
 		return (NULL);
-	new->n = n;
 	tmp = *head;
 	while (position < idx)
 	{

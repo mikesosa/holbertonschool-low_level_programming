c27c875a4e48912c8f65349a18764dfed4100045
mikesosa <micaelsosa@fernandosor.edu.co>
2019-08-06 10:55:36 +0000

    Secure guards cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,10 +12,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp;
 	unsigned int position = 1;/*It stars counting positions at 1*/
 
-	if (!(*head))
-		return (NULL);
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (!new || !head)
+		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	if (*head == 0)
 		return (NULL);
 	new->n = n;
 	tmp = *head;

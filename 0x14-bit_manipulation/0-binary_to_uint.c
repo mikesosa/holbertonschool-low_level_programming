aaefcc097323763423b2cb5e36be4df802a578f0
mikesosa <micaelsosa@fernandosor.edu.co>
2019-08-08 11:57:52 +0000

    I like this, maybe I'm over complecating... but it works just fine

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+/**
+ * _strlen - finding the lenght of a string.
+ * @s: first pointer
+ * Return: it returns the length of the string.
+ */
+int _strlen(char *s)
+{
+	int str;
+
+	str = 0;
+	while (s[str] != '\0')
+		str++;
+	return (str);
+}
+/**
+ * x_to_the_n - Fucntion that does to the power of
+ * @x: the number
+ * @n: the eleveado
+ * Return:@number the result
+ */
+int x_to_the_n(int x, int n)
+{
+	int i;
+	int number = 1;
+
+	for (i = 0; i < n; ++i)
+		number *= x;
+	return (number);
+}
+/**
+ * binary_to_uint - Converts binary to decimals
+ * @b: pointer to char given by main
+ * Return:@result the result
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int j = 0, operand = 0, len = 0;
+	unsigned int result = 0;
+
+	len = _strlen((char *) b);
+	if (b == NULL)
+		return (0);
+	while (b[j] != '\0')
+	{
+		if ((b[j] != 49) && (b[j] != 48))
+			return (0);
+		operand = x_to_the_n(2, j);
+		if (b[len - 1] == 49)/*Ascci code of 1*/
+			result += operand;
+		len--;
+		j++;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,22 @@
+<h1>Bit Manipulation</h1>
+<h2>Description</h2>
+<p>Finally the bitwise operators... I wanted to know about this, since I stared</p>
+<strong>Mike Sosa</strong>
+<br><br>
+<caption>Files and Description</caption>
+<table style="width:100%">
+	<tr>
+		<th>File</th>
+		<th>Description</th>
+	</tr>
+	<tr>
+		<td>_putchar.c</td>
+		<td>Writes a character to the standard output.</td>
+	</tr>
+	<tr>
+		<td>0-binary_to_uint.c</td>
+		<td>Converts Binary to decimal</td>
+	</tr>
+</table>
+<br>
+<strong>Author: <a href="https://twitter.com/micael_sosa" target="_blank">Michael Sosa </a></strong><small>for Holberton School 2019</small>

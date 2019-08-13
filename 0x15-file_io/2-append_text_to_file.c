04893edb33d651f47e00375537268dd02e1b51e1
mikesosa <micaelsosa@fernandosor.edu.co>
2019-08-13 20:30:41 +0000

    Appending text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,30 @@
 #include "holberton.h"
 /**
- * create_file - Creates a file and inserts text into it
+ * append_text_to_file - Appends text to a precreated file
  * @filename: the name of the file passed as argument
  * @text_content: text to write into the file
  * Return: @1 if success and @-1 if fails
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int w, file, cnt = 0;
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_RDWR | O_APPEND);
 	if (file == -1)
 	{
 		close(file);
 		return (-1);
 	}
-	if (text_content == NULL)
-		w = write(file, "", 0);
-	else
+	if (file == 1 && text_content == NULL)
 	{
-		while (text_content[cnt])
-			cnt++;
-		w = write(file, text_content, cnt);
+		close(file);
+		return (1);
 	}
+	while (text_content[cnt])
+		cnt++;
+	w = write(file, text_content, cnt);
 	if (w == -1)
 	{
 		close(file);

6b15c385558f492551dec023b237dfc5632fa19e
mikesosa <micaelsosa@fernandosor.edu.co>
2019-08-13 20:00:02 +0000

    If text_content NULL create an empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,25 @@ int create_file(const char *filename, char *text_content)
 	int w, file, cnt = 0;
 
 	if (!filename)
-		return (0);
+		return (-1);
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 	{
 		close(file);
-		return (0);
+		return (-1);
 	}
-	while (text_content[cnt])
-		cnt++;
-	w = write(file, text_content, cnt);/*printf not allowed*/
+	if (text_content == NULL)
+		w = write(file, "", 0);
+	else
+	{
+		while (text_content[cnt])
+			cnt++;
+		w = write(file, text_content, cnt);/*printf not allowed*/
+	}
 	if (w == -1)
 	{
 		close(file);
-		return (0);
+		return (-1);
 	}
 	close(file);
 	return (1);

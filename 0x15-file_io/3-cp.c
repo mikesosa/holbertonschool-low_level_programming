60724276b1bea7da0eb95a63cd7a8e60441cf3c8
mikesosa <micaelsosa@fernandosor.edu.co>
2019-08-14 00:46:43 +0000

    CHA CHA CHA CHAAAAANNN -> FINALLY

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-void argument_error97(int arguments)
+/**
+ * error97 - prints error if bad usage
+ * @arguments: list of arguments
+ * Return: void
+ */
+void error97(int arguments)
 {
 	if (arguments != 3)
 	{
@@ -7,59 +12,71 @@ void argument_error97(int arguments)
 		exit(97);
 	}
 }
-
-
-void read_error98(char *file)
+/**
+ * error98 - prints error if can't read from file
+ * @file: file implicated
+ * Return: void
+ */
+void error98(char *file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 	exit(98);
 }
-
-void write_error99(char *file)
+/**
+ * error99 - prints error if can't write file
+ * @file: file implicated
+ * Return: void
+ */
+void error99(char *file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 	exit(99);
 }
-
-void close_error100(int file)
+/**
+ * error100 - prints error if can't close file
+ * @file: file implicated
+ * Return: void
+ */
+void error100(int file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
 	exit(100);
 }
+/**
+ * main - functian that copies the content to other file
+ * @argc: ammount of arguments
+ * @argv: list of agouments
+ * Return: 98 99 100 if fails @0 if works
+ */
 int main(int argc, char **argv)
 {
-	int file;
-	int file2;
-	int reading = -1;
-	int writing;
-	int closing;
+	int file, file2, w, c, r = -1;
 	char s[1024] = {0};
 
-	argument_error97(argc);
+	error97(argc);
 	file = open(argv[1], O_RDONLY);
 	if (file == -1)
-		read_error98(argv[1]);
+		error98(argv[1]);
 	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
-		write_error99(argv[2]);
-	while(reading)
+		error99(argv[2]);
+	while (r)
 	{
-		reading = read(file, s, 1024);
-		if (reading == -1)
-			read_error98(argv[1]);
-		if (reading)
+		r = read(file, s, 1024);
+		if (r == -1)
+			error98(argv[1]);
+		if (r)
 		{
-			writing = write(file2, s, reading);
-			if (writing == -1)
-				write_error99(argv[2]);
+			w = write(file2, s, r);
+			if (w == -1)
+				error99(argv[2]);
 		}
 	}
-	closing = close(file);
-	if (closing == -1)
-		close_error100(file);
-	closing = close(file2);
-	if (closing == -1)
-		close_error100(file2);
+	c = close(file);
+	if (c == -1)
+		error100(file);
+	c = close(file2);
+	if (c == -1)
+		error100(file2);
 	return (0);
 }
-

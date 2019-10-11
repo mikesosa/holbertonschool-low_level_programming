801bf9e4999ef811d9af6f99a90c2d0f0baea24e
Michael <michael.sosa@holbertonschool.com>
2019-10-11 17:03:42 -0500

    Printing a hast table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+* hash_table_print - prints a hash table
+* @ht: the hash table
+*
+* Return: void
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp = NULL;
+	unsigned int index = 0;
+	char *s = "";
+
+	putchar('{'); /* doing this for the format */
+
+	while (index < ht->size)
+	{ /* loop as many buckets as the whole hash table */
+		temp = ht->array[index];
+		if (temp) /* if the bucket is not empty, */
+			printf("%s", s); /* prints nothing until we actually printed */
+		while (temp) /* go through the bucket */
+		{ /* print the key and the value */
+			printf("'%s': '%s'", temp->key, temp->value);
+			if (temp->next) /* if there is more, print puncuation */
+				printf("%s", s);
+			temp = temp->next;
+			s = ", "; /* assigns the comma once we found a non empty index */
+		}
+		index++;
+	}
+	putchar('}');
+	putchar(10);
+}
+

88c3ad7c09436bcc0fe5f14f7dc897d2c7c84df0
Michael <michael.sosa@holbertonschool.com>
2019-10-11 16:04:57 -0500

    Getting the value using a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *check = NULL;
+
+	if (!ht || !key || !strcmp(key, ""))
+		return (NULL); /* edge case checks */
+
+	index = key_index((const unsigned char *)key, ht->size);
+	
+	check = ht->array[index];
+	while (check)
+	{ /* check all the linked lists in this index */
+		if (!strcmp(key, check->key))
+			return (check->value); /* found match, return */
+		check = check->next;
+	}
+	return (NULL); /* no key like that in tha array */
+}

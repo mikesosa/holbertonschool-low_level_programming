e828345cd9faca034142121ef1634720bcc78d58
Michael <michael.sosa@holbertonschool.com>
2019-10-11 15:36:31 -0500

    Test

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *check = NULL;
+	hash_node_t *new = NULL;
+
+
+	if (!ht || !key || !(*key) || !strcmp(key, "") || !value)
+		return (0); /* if null hash table, key, or empty key, error */
+
+	index = key_index((unsigned char *)key, ht->size);
+	check = ht->array[index];
+	
+	while (check) /* check if the key already exists, if so replace. */
+	{
+		if (!strcmp(key, check->key)) /* strcmp returns 0 if matches */
+		{
+			printf("%s and %s", key, value);
+			free(check->value); /* get rid of old value, replace it */
+			check->value = strdup(value); /* str dup reallocs */
+			if (check->value)
+				return (1);
+			return (0);
+		}
+		check = check->next;
+	}
+	new = malloc(sizeof(hash_node_t)); /* malloc a node */
+	if (!new) /* malloc error */
+		return (0);
+
+	new->key = strdup(key); /* dup key */
+	new->value = strdup(value); /* dup value */
+
+	new->next = ht->array[index]; /* make the new node be the first */
+	ht->array[index] = new;
+	return (1);
+}

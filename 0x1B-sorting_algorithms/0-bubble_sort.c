7bfa2c7feee4647fc1d52d15f68fa17d1f5ebe8d
Michael <michael.sosa@holbertonschool.com>
2019-10-29 14:11:22 -0500

    Bubble sorting - first task

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+* bubble_sort - implements the bubble sort algo for an array of ints
+* @array: the array we are iterating through
+* @size: the size of the array
+*
+* Return: none it is void
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0;
+	int tmp = 0;
+	bool sorted = true;
+
+	while (sorted)
+	{
+		sorted = false;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				sorted = true;
+			}
+			i++;
+		}
+	}
+}

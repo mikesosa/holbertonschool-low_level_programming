93bbb4a0f8f5f522923d07b587589e5dcf0f9dcb
Michael <michael.sosa@holbertonschool.com>
2019-10-30 08:20:04 -0500

    Counting sort

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -4,6 +4,26 @@
 #include "sort.h"
 
 /**
+ * filling_array - filling out the old array in a sorted way
+ * @array: old array
+ * @new_array: new array
+ * @k: size of the array
+ */
+void filling_array(int *array, int *new_array, int k)
+{
+	int last_n = 0, s_position = 0, j;
+
+	for (j = 0; j <= k; j++)
+	{
+		if (new_array[j] > last_n)
+		{
+			array[s_position] = j;
+			last_n = new_array[j];
+			s_position++;
+		}
+	}
+}
+/**
  * counting_sort - sorts an array based on number of coincidences
  * @array: array given by main
  * @size: size of array
@@ -11,7 +31,7 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int i = 0, j = 0, lenght = size, r = 0, k = 0, coincidences, s_position = 0;
+	int i = 0, j = 0, lenght = size, k = 0;
 	int *new_array;
 	size_t size2, x;
 
@@ -22,6 +42,7 @@ void counting_sort(int *array, size_t size)
 		i++;
 		lenght--;
 	}
+
 	size2 = k;
 	new_array = malloc(sizeof(int) * k);
 	memset(new_array, 0, size2);
@@ -31,23 +52,18 @@ void counting_sort(int *array, size_t size)
 		{
 			if (array[x] == j)
 				new_array[j] += 1;
-		} }
+		}
+	}
 	for (j = 0; j <= k; j++)
 	{
-		r += new_array[j];
-		printf("%d", r);
+		new_array[j] += new_array[j - 1];
+		printf("%d", new_array[j]);
 		if (j < k)
 			printf(", ");
 		else
 			printf("\n");
-		if (new_array[j] != 0)
-		{
-			i = 0;
-			coincidences = new_array[j];
-			while (i < coincidences)
-			{
-				array[s_position] = j;
-				s_position++;
-				i++;
-			} } }
+	}
+
+	filling_array(array, new_array, k);
+	free(new_array);
 }

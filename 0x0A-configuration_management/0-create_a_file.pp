7af011ca923fe013afd16434ecd6c23c0e9cc2d9
Michael <michael.sosa@holbertonschool.com>
2019-12-09 08:50:38 -0500

    Wrong folder

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# Creating a file in /tmp using puppet
-file { '/tmp/holberton':
-path      => '/tmp/holberton',
-mode      => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-  content => 'I love Puppet'
-}
